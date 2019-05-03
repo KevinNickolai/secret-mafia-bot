8b8833070b2ec7945617555804216b9657dd1a38
KevinNickolai <nickola6@msu.edu>
2019-05-03 04:12:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 //Created from tutorial on
 //https://medium.freecodecamp.org/how-to-create-a-discord-bot-under-15-minutes-fb2fd0083844
 
-require('dotenv').config()
-const Discord = require('discord.js')
-const fs = require('fs')
-const client = new Discord.Client()
+require('dotenv').config();
+const Discord = require('discord.js');
+const fs = require('fs');
+const client = new Discord.Client();
 
 fs.readdir('./events/', (err, files) => {
   files.forEach(file => {
-    const eventHandler = require(`./events/${file}`)
-	const eventName = file.split('.')[0]
-    client.on(eventName, (...args) => eventHandler(client, ...args))
-  })
-})
+    const eventHandler = require(`./events/${file}`);
+	const eventName = file.split('.')[0];
+    client.on(eventName, (...args) => eventHandler(client, ...args));
+  });
+});
 /**
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })*/
 
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN).catch('failed to login');
