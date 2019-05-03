c25e5e006bf8b9f4cf93eb667adb38d508e834e3
KevinNickolai <nickola6@msu.edu>
2019-05-03 04:00:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 
 require('dotenv').config()
 const Discord = require('discord.js')
-const client = new Discord.Client()
 const fs = require('fs')
+const client = new Discord.Client()
+
 fs.readdir('./events/', (err, files) => {
   files.forEach(file => {
     const eventHandler = require(`./events/${file}`)
@@ -12,9 +13,9 @@ fs.readdir('./events/', (err, files) => {
     client.on(eventName, (...args) => eventHandler(client, ...args))
   })
 })
-
+/**
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
-})
+})*/
 
 client.login(process.env.BOT_TOKEN)
