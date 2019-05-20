e5cbfe47c34269bc41296913cf14aa9208271113
KevinNickolai <nickola6@msu.edu>
2019-05-20 19:02:44 -0400

    Updated database disconnection handling

--- classes/databaseWrapper.js
+++ classes/databaseWrapper.js
@@ -21,7 +21,7 @@ class DatabaseWrapper {
 		this.connection.on('error', function(err){
 			console.log("Database Error", err);
 			if(err.code === 'PROTOCOL_CONNECTION_LOST'){
-				handleDisconnect();
+				this.handleDisconnect();
 			} else{
 				throw err;
 			}
